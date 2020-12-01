967ff526d7bbd5868bfb4d008fc7a7561fbecd30
icwet <icwet@yandex.ru>
2020-12-01 03:37:34 +0300

    complete git log output

--- server/src/services/git.ts
+++ server/src/services/git.ts
@@ -1,4 +1,5 @@
 import { readdir } from "fs/promises";
+import { spawn } from "child_process";
 import db from "loaders/lowdb";
 
 export const getRepos = async (path: string): Promise<string[]> => {
@@ -6,9 +7,43 @@ export const getRepos = async (path: string): Promise<string[]> => {
   db.set("repos", files);
   return files;
 };
-export const getCommits = () => {};
-export const getDiff = () => {};
-export const getRepositoryContent = () => {};
-export const getFileContent = () => {};
-export const deleteRepository = () => {};
-export const downloadRepository = () => {};
+export const getCommits = (path: string, repoName: string, hash: string, page?: string): Promise<string> => {
+  const param = "^^param^^";
+  const line = `^^line^^`;
+  const prettyFormat = `%H${param}%b${param}%cd${line}`;
+  return new Promise((resolve, reject) => {
+    const log = spawn("git", ["log", "-n40", `--pretty=format:${prettyFormat}`, hash], { cwd: `${path}/${repoName}` });
+    let result = "";
+    log.stdout.on("data", (chunk) => {
+      result += chunk.toString();
+    });
+    log.on("close", () => {
+      const parseLog = result
+        .replace(/\\\\n/, "")
+        .split("\n")
+        .join("")
+        .split(line)
+        .filter(Boolean)
+        .map((commit) => {
+          const [hash, message, date] = commit.split(param);
+          return {
+            hash,
+            message,
+            date,
+          };
+        });
+      resolve(JSON.stringify(parseLog));
+    });
+    log.stderr.on("data", (data) => {
+      reject(`${data}`);
+    });
+    log.on("error", (err) => {
+      reject(`${err}`);
+    });
+  });
+};
+export const getDiff = (path: string, repoName: string, hash: string) => {};
+export const getRepositoryContent = (path: string, repoName: string, hash: string, dirictory: string) => {};
+export const getFileContent = (path: string, repoName: string, hash: string, fileName: string) => {};
+export const deleteRepository = (path: string, repoName: string) => {};
+export const downloadRepository = (path: string, url: string) => {};
--- server/tests/services/git.test.ts
+++ server/tests/services/git.test.ts
@@ -19,46 +19,45 @@ describe("Service Git", () => {
     });
   });
   describe("getCommits", () => {
-    it("Should return commit list with date and name", () => {
+    it("Should return commit list with date and name", async () => {
       const commits = await getCommits(pathToRepo, "preact", "master");
-      expect(commits).to.deep.include({
-        commit: "0fc3b21acc5f044e0e676d3be547313b026beded",
-        abbreviated_commit: "0fc3b21a",
-        tree: "70271733e28c0b35eefa3829a4c69a49d99b64ce",
-        abbreviated_tree: "70271733",
-        parent: "a6e15286ca845491c6c942bbea1104acde8ecff4",
-        abbreviated_parent: "a6e15286",
-        refs: "",
-        encoding: "",
-        subject: "Checking for null explicitly",
-        sanitized_subject_line: "Checking-for-null-explicitly",
-        body: "",
-        commit_notes: "",
-        verification_flag: "N",
-        signer: "",
-        signer_key: "",
-        author: {
-          name: "rafi993",
-          email: "rafi993@protonmail.com",
-          date: "Wed, 25 Nov 2020 17:36:07 +0530",
-        },
-        commiter: {
-          name: "rafi993",
-          email: "rafi993@protonmail.com",
-          date: "Wed, 25 Nov 2020 17:36:07 +0530",
-        },
-      });
+      expect(commits)
+        .to.be.an("string")
+        .that.does.include(
+          '{"hash":"f955cfcceb50d504faa7c05a95065512cf900e57","message":"Adding defaultValue support in select tag","date":"Wed Nov 25 16:18:58 2020 +0100"'
+        );
     });
   });
   describe("getDiff", () => {
-    it("Should return diff in one line", () => {
-      const diff = getDiff(pathToRepo, "preact", "master");
+    it("Should return diff in one line", async () => {
+      const diff = await getDiff(pathToRepo, "preact", "master");
       expect(diff).to.have.string("b/benches/src/03_update10th1k_x16.html");
     });
   });
   describe("getRepositoryContent", () => {
-    it("Should return file list", () => {
-      const files = getRepositoryContent();
+    it("Should return file list", async () => {
+      const files = await getRepositoryContent(pathToRepo, "preact", "f955cfcc", "src/");
+      expect(files).to.be.an("array").that.does.include("src/create-element.js");
+    });
+  });
+  describe("getFileContent", () => {
+    it("Should return file content", async () => {
+      const content = await getFileContent(pathToRepo, "preact", "f955cfcc", "src/component.js");
+      expect(content).to.have.string("export function Component(props, context) {");
+    });
+  });
+  describe("deleteRepository", () => {
+    it("Should return delete repository name", async () => {
+      // @ts-ignore
+      const deleteRepository = await deleteRepository(pathToRepo, "preact");
+      expect(deleteRepository).to.have.string("Repository 'preact' delete");
+    });
+  });
+  describe("downloadRepository", () => {
+    it("Should return download repository name", async () => {
+      // @ts-ignore
+      const downloadRepository = await downloadRepository(pathToRepo, "react");
+      expect(downloadRepository).to.have.string("Repository 'react' download");
     });
   });
 });
